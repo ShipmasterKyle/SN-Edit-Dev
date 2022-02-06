1f1aefbcc068a5eb56a901902ca20462d0bf8258
ShipmasterKyle <silverteamroblox@gmail.com>
2022-02-06 11:13:05 -0500

    My dumb self forgot to remove a tw reference when I ~~stole~~ borrowed some code from Turbowarp

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -2,12 +2,18 @@ import projectData from './project-data';
 
 /* eslint-disable import/no-unresolved */
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
-import costume1 from '!raw-loader!./dango.svg';
+import costume1 from '!raw-loader!./sundae.svg';
 /* eslint-enable import/no-unresolved */
-import {TextEncoder} from '../tw-text-encoder';
 
 const defaultProject = translator => {
-    const encoder = new TextEncoder();
+    let _TextEncoder;
+    if (typeof TextEncoder === 'undefined') {
+        _TextEncoder = require('text-encoding').TextEncoder;
+    } else {
+        /* global TextEncoder */
+        _TextEncoder = TextEncoder;
+    }
+    const encoder = new _TextEncoder();
 
     const projectJson = projectData(translator);
     return [{
