3196a6fcaa978e061d36c989fa8feea39620c3a0
ShipmasterKyle <silverteamroblox@gmailcom>
2022-03-31 15:01:20 +0000

    Build for 4437b62cf68eecdb85d847ad1d955ac3caded41e [skip ci]

---
+++ static/addons/libraries/common/global-theme.js
@@ -0,0 +1,35 @@
+export default function () {
+  const now = Date.now() / 1000;
+  if (now < 1648911600 && now > 1648738800 && ["settings", "popup"].includes(location.pathname.split("/")[2]))
+    document.documentElement.style.setProperty("--brand-orange", "#568203");
+
+  const lightThemeLink = document.createElement("link");
+  lightThemeLink.setAttribute("rel", "stylesheet");
+  lightThemeLink.setAttribute("href", chrome.runtime.getURL("/webpages/styles/colors-light.css"));
+  lightThemeLink.setAttribute("data-below-vue-components", "");
+  lightThemeLink.media = "not all";
+  document.head.appendChild(lightThemeLink);
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(["globalTheme"], ({ globalTheme = false }) => {
+      // true = light, false = dark
+      if (globalTheme === true) {
+        lightThemeLink.removeAttribute("media");
+      }
+      let theme = globalTheme;
+      resolve({
+        theme: globalTheme,
+        setGlobalTheme(mode) {
+          if (mode === theme) return;
+          chrome.storage.sync.set({ globalTheme: mode }, () => {
+            if (mode === true) {
+              lightThemeLink.removeAttribute("media");
+            } else {
+              lightThemeLink.media = "not all";
+            }
+          });
+          theme = mode;
+        },
+      });
+    });
+  });
+}
