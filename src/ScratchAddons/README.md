2b82952b8f61bf1a2ea0d62f484afc00f17ce242
Ship <79712264+ShipmasterKyle@users.noreply.github.com>
2022-03-31 11:40:29 -0400

    Delete src/ScratchAddons directory

--- src/ScratchAddons/README.md
+++
@@ -1,5 +0,0 @@
-# Hacky compatibility for Scratch Addons
-
-If Babel and whatnot does not COOPERATE with Scratch Addons, I will FORCE it to.
-
-All files in this folder are licensed under the GNU General Public License v3.0:
--- src/ScratchAddons/addons/comments-linebreaks/userscript.js
+++
@@ -1,9 +0,0 @@
-import formatProfileComments from "../../libraries/common/cs/format-profile-comments.js";
-
-export default async function ({ addon, global, console }) {
-  while (true) {
-    const comment = await addon.tab.waitForElement(".comment .content", { markAsSeen: true });
-    comment.style.whiteSpace = "break-spaces";
-    formatProfileComments(comment);
-  }
-}
